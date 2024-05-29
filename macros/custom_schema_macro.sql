eaa6b8153ae0f4eaf481b3181f13dca68fca7a1e
sravan kumar <sravankc31@gmail.com>
2024-05-29 11:42:48 +0000

    proj

--- macros/custom_schema_macro.sql
+++ macros/custom_schema_macro.sql
@@ -5,7 +5,10 @@
 
         {{ default_schema }}
     {% elif   target.schema == 'prod' -%}
-       "prod"_{{ custom_schema_name | trim }}
+       prod_{{ custom_schema_name | trim }}
+        {% elif   target.schema == 'qa' -%}
+       qa_{{ custom_schema_name | trim }}
+
 
     {%- else -%}
 
